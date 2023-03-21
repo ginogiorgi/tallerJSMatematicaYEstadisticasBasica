acf6e21d89ef0fe4b49558736a9ce99c7dd6a193
ginogiorgi <ginorubengiorgi@gmail.com>
2023-03-21 10:49:32 -0300

    Clase 9 Taller Práctico de JavaScript: Matemáticas y Estadística Básica

---
+++ cupones.js
@@ -0,0 +1,6 @@
+const cupones = {
+    "Descuento30": 30,
+    "Descuento60": 60,
+    "Descuento40": 40,
+    "free": 100,
+}
--- descuentos.js
+++ descuentos.js
@@ -6,31 +6,18 @@ const pResult = document.querySelector('#result');
 function calcularPrecioConDecuento(){
     const precio = Number(inputPrice.value);
     const cupon = inputCoupon.value;
-    let descuento;
     
     if(!precio || !cupon){
         pResult.innerHTML = "Por favor rellena el formulario";
         return;
     }
-    switch (cupon){
-       case 'descuento30': descuento = 30;
-       break;
-       case 'descuento70': descuento = 70;
-       break;
-       case 'free': descuento = 100; 
-       break;
-       default: pResult.innerHTML = "El codigo de descuento no es valido";
-       return;
-    }
-    if (descuento <= 100){
+    if (cupones[cupon]){
+        let descuento = cupones[cupon];
         const nuevoPrecio = (precio * (100 - descuento)) / 100;
         pResult.innerHTML = "El nuevo precio con descuento es: " + nuevoPrecio;
         }
     else{
-        pResult.innerHTML = "El descuento no puede ser mayor que 100";
+        pResult.innerHTML = "El codigo de descuento no es valido";
     };
 }
-
-
-
 button.addEventListener('click', calcularPrecioConDecuento);
