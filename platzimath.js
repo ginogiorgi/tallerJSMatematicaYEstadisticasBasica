0ca1ff7c660ce418b1799fa1ec33a2f6d8de953c
ginogiorgi <ginorubengiorgi@gmail.com>
2023-03-22 17:50:12 -0300

    Clase 17 Taller Práctico de JavaScript: Matemáticas y Estadística Básica

--- platzimath.js
+++ platzimath.js
@@ -25,3 +25,18 @@ function ordenarLista(listaDesordenada){
     const lista = listaDesordenada.sort((a, b) => a-b);
     return lista;
 };
+function calcularModa(lista) {
+    const listaCount = {};
+
+    for (let i = 0; i < lista.length; i++){
+        const elemento = lista[i];
+
+        if (listaCount[elemento]){
+            listaCount[elemento] += 1;
+        }
+        else {
+            listaCount[elemento] = 1;
+        };
+    };
+    console.log(listaCount);
+}
