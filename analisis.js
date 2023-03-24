c8293e560115a8889340f0ba8878c6b616c41a00
ginogiorgi <ginorubengiorgi@gmail.com>
2023-03-24 18:49:07 -0300

    Clase 24  Taller Práctico de JavaScript: Matemáticas y Estadística Básica

--- analisis.js
+++ analisis.js
@@ -6,3 +6,13 @@ function medianaPorId(idPersonaBuscada){
     const salarios = trabajos.map((elemento) => elemento.salario);
     return PlatziMath.calcularMediana(salarios);
 }
+
+function proyeccionPorPersona(idPersonaBuscada){
+    let porcentajesCrecimiento = [];
+    const trabajos = encontrarPersonaPorId(idPersonaBuscada).trabajos;
+
+    for (let i = 1; i < trabajos.length; i++){
+        porcentajesCrecimiento.push((trabajos[i].salario - trabajos[i - 1].salario) / trabajos[i - 1].salario );
+    };
+    return trabajos[trabajos.length - 1].salario * PlatziMath.calcularMediana(porcentajesCrecimiento) + trabajos[trabajos.length - 1].salario;
+}
